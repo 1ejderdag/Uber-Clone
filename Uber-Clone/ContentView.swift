3e4e386040c10594fc0fa1d34171ccd609fb9e28
Ejder Dag <ejdrdag@gmail.com>
2024-07-09 04:44:20 +0300

    initial commit

---
+++ Uber-Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uber-Clone
+//
+//  Created by Ejder Dağ on 9.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uber-Clone/Uber_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_CloneApp.swift
+//  Uber-Clone
+//
+//  Created by Ejder Dağ on 9.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Uber_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
