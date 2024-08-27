0a6ed106260ac6641522e45a86110666d47e7a79
Ejder Dag <ejdrdag@gmail.com>
2024-08-27 03:25:34 +0300

    find coordinates

--- Uber-Clone/Core/Home/Views/UberMapViewRepresentable.swift
+++ Uber-Clone/Core/Home/Views/UberMapViewRepresentable.swift
@@ -24,8 +24,8 @@ struct UberMapViewRepresentable: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        if let selectedLocation = locationSearchViewModel.selectedLocation {
-            print("DEBUG: Selected location in map view \(selectedLocation)")
+        if let coordinate = locationSearchViewModel.selectedLocationCoordinate {
+            print("DEBUG: Selected coordinates in map view \(coordinate)")
         }
     }
     
